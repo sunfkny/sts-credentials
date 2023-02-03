3d97446552aeb508bc65789f02e34f3a3a293821
sunfkny <30853461+sunfkny@users.noreply.github.com>
2023-02-03 20:37:46 +0800

    0.03

--- src/sts_credentials/__init__.py
+++ src/sts_credentials/__init__.py
@@ -2,7 +2,7 @@ from sts_credentials.credentials.oss import get_credential_oss
 from sts_credentials.credentials.cos import get_credential_cos
 from sts_credentials.utils.decorators import ttl_lru_cache
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __all__ = [
     "get_credential_oss",
     "get_credential_cos",
